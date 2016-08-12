b2104d2d7cca625fff971215547b327d25ed55dc
neethu93 <pingneethu@gmail.com>
2016-08-12 14:04:12 +0200

    view things changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ frank = User.create( email: 'frank@ex.com', password: 'abcd1234' )
 alice = User.create( email: 'alice@ex.com', password: 'abcd1234' )
 anton = User.create( email: 'anton@ex.com', password: 'abcd1234' )
 
-Cat.create( { name: "Amy", age: 3, color: "orange", gender:  Gender::FEMALE, image_url: "https://scontent.xx.fbcdn.net/v/t1.0-9/1604778_3793443852762_400933829_n.jpg?oh=9403068fb9e803dfd3bccd0d1b9d7eeb&oe=581B54F5", price: 30, description: "Super sporty, guaranteed fun time."} )
+Cat.create( { name: "Amy", age: 3, color: "orange", gender:  Gender::FEMALE, image_url: "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg", price: 30, description: "Super sporty, guaranteed fun time."} )
 Cat.create( { name: "Ruby", age: 2, color: "light grey", gender:  Gender::FEMALE, image_url: "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg", price: 100, description: "Smart kitty, she can help you coding."} )
 Cat.create( { name: "Garfield", age: 5, color: "grey", gender:  Gender::MALE, image_url: "http://wallpaper.pickywallpapers.com/1920x1080/beautiful-cat-with-blue-eyes.jpg", price: 35, description: "A bit naughty but his beautiful eyes..."} )
 Cat.create( { name: "Snoopy", age: 1, color: "orange", gender: Gender::MALE, image_url: "https://i.ytimg.com/vi/XLFqNc0_Dp8/maxresdefault.jpg", price: 45, description: "Fluffy kitten, waiting to cuddle."} )
